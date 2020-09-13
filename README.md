1b5ff39d4d038463f0fa8d9a8eb9b4bb1d3c0a11
YesmynameisPerry <jtaylor@seek.com.au>
2020-09-13 14:59:29 +1000

    fixup

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pez Logger
+
+I'm Pez, this is my logger.
+
+It logs things.
+
+It also has a mock logger for tests.
--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
   description='Custom logger',   # Give a short description about your library
   author='Jason \'Perry\' Taylor',                   # Type in your name
   author_email='yesmynameisperry@gmail.com',      # Type in your E-Mail
-  url='https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url='https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
+  url='https://github.com/YesmynameisPerry/pezLogger',   # Provide either the link to your github or to your website
+  download_url='https://github.com/YesmynameisPerry/pezLogger/archive/1.0.tar.gz',    # I explain this later on
+  keywords=['LOGGER', 'CUSTOM', 'PEZ'],   # Keywords that define your package best
   install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
